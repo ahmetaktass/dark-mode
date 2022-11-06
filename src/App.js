8ed7c7fac065aeb1b7daf64c359f920d5319629c
Ahmet Aktaş <mail.ahmetaktas@gmail.com>
2022-11-07 02:08:06 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import './App.css'
+import SunIcon from './assets/sun.svg'
+import MoonIcon from './assets/moon.svg'
 
 function App() {
+  const [theme, setTheme] = useState('light')
+
+  const handleClick = () => {
+    console.log('theme')
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+  useEffect(() => {
+    console.log('theme', theme)
+    document.body.className = theme
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`app ${theme} `}>
+      <span className="span" onClick={handleClick}>
+        {theme === 'light' ? (
+          <img className="icon" src={SunIcon} alt="" />
+        ) : (
+          <img className="icon" src={MoonIcon} alt="" />
+        )}
+      </span>
+      <div>
+        {' '}
+        {theme === 'light' ? <h1>Light Mode </h1> : <h1>Dark Mode </h1>}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
